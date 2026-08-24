bde69ca3424bd22369677521989cd0291da2e05b
Daniel da Costa Brant <Daniel55779296@edu.df.senac.br>
2026-08-24 14:30:14 -0300

    Adicionando imagens aos produtos

--- js/script.js
+++ js/script.js
@@ -50,105 +50,71 @@ async function carregarServicos() {
     }
 }
 
-
 /*============================================================
     4) FUNÇÃO PARA CRIAR OS CARDS NA TELA
 ============================================================*/
 
 function renderizarServicos(lista) {
 
-
     // Verifica se a página possui a lista de produtos
     if (!ListaServicos) {
         return;
     }
 
-
     // LIMPA A TELA
     ListaServicos.innerHTML = "";
 
-
     // CRIA OS CARDS
     lista.forEach(servico => {
-
 
         const card = document.createElement("div");
 
         card.classList.add("card");
 
-
         card.innerHTML = `
-
             <h3>${servico.titulo}</h3>
-
-
             <img 
                 src="${servico.img}" 
                 width="150" 
                 height="150"
                 alt="${servico.titulo}"
             >
-
-
             <p>
                 ${servico.descricao}
             </p>
-
-
             <p>
                 <strong>Preço:</strong> 
                 R$ ${servico.preco.toFixed(2)}
             </p>
-
-
             <a href="../pages/detalhes-cardapio.html?id=${servico.id}">
                 <button>
                     Ver detalhes
                 </button>
             </a>
-
         `;
 
-
         ListaServicos.appendChild(card);
-
     });
-
 }
-
 
 /*============================================================
     5) BUSCA DE SERVIÇOS (CAFÉS)
 ============================================================*/
 
-
 if (buscaServicos) {
-
 
     buscaServicos.addEventListener("input", function () {
 
-
         const texto = buscaServicos.value.toLowerCase();
 
-
-
         const filtrados = servicos.filter((servico) => {
-
 
             return servico.titulo
                 .toLowerCase()
                 .includes(texto);
-
-
         });
-
-
-
         renderizarServicos(filtrados);
-
-
     });
-
 }
 
 
